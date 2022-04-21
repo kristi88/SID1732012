d764871fffefc00a74c74e51402eb2bff136dbd7
kristi88 <37550269+kristi88@users.noreply.github.com>
2022-04-21 09:50:10 -0700

    WEEK 4
    WEEK 4

--- script.py
+++ script.py
@@ -1,19 +1,29 @@
-from netmiko import Netmiko
 import json
 
-with open('R1.json') as f:
-  data = json.load(f)
+class Router:
+# Use python constructor to avoid the attribute mistakes
+    def __init__(self, hostname, enable, motd, encryption, ip_add, username, password):
+        self.hostname = hostname
+        self.enable = enable
+        self.ip_add = ip_add
+        self.motd = motd
+        self.encryption = encryption
+        self.username = username
+        self.password = password
 
+    def router_configuation(self):
+        print("Configuring the R1 Router to following settings\n")
+        print(self.hostname)
+        print(self.ip_add)
+        print(self.motd)
+        print(self.encryption)
+        print(self.username)
+        print(self.password)
 
-net_connect = Netmiko(
-   data["ip_addr"],
-   username=data["username"],
-   password=data["password"],
-   device_type=data["device_type"],
-)
+if __name__ == '__main__':
+    with open('R1.json') as f:
+        data = json.load(f)
 
-# Running the commands on the remote router.
-command = "show ip route connected"
-output = net_connect.send_command(command)
-net_connect.disconnect()
-print(output)
+    r1 = Router(data['hostname'], data['enable'], data['motd'], data['encryption'], data['ip_address'], data['username'], data['password'])
+
+    r1.router_configuation()
